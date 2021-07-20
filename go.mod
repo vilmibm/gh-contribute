d3058e27412c2d5998f1ea9a16073a2c44966917
nate smith <vilmibm@github.com>
2021-07-20 13:43:16 -0500

    copypasta

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/vilmibm/gh-contribute
+
+go 1.16
+
+require github.com/cli/safeexec v1.0.0
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+
+	"github.com/cli/safeexec"
+)
 
 func main() {
 	fmt.Println("TODO")
+}
+
+// gh shells out to gh, returning STDOUT/STDERR and any error
+func gh(args ...string) (sout, eout bytes.Buffer, err error) {
+	ghBin, err := safeexec.LookPath("gh")
+	if err != nil {
+		err = fmt.Errorf("could not find gh. Is it installed? error: %w", err)
+		return
+	}
+
+	cmd := exec.Command(ghBin, args...)
+	cmd.Stderr = &eout
+	cmd.Stdout = &sout
+
+	err = cmd.Run()
+	if err != nil {
+		err = fmt.Errorf("failed to run gh. error: %w, stderr: %s", err, eout.String())
+		return
+	}
+
+	return
 }
