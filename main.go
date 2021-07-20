55d57f1ac38e53ed64fe0820b45a35ae6190c492
nate smith <vilmibm@github.com>
2021-07-20 17:52:20 -0500

    wip

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/cli/safeexec"
 	"github.com/spf13/cobra"
@@ -43,20 +45,59 @@ func resolveRepository() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	viewOut := sout.String()
-	viewOut = strings.Split(viewOut, "\n")[0]
+	viewOut := strings.Split(sout.String(), "\n")[0]
 	repo := strings.TrimSpace(strings.Split(viewOut, ":")[1])
 
 	return repo, nil
 }
 
 func runContribute(opts contributeOpts) error {
-	fmt.Println(opts.Repository)
-	sout, _, err := gh("issue", "list")
+	hwIssues, err := issuesByLabel(opts.Repository, "help wanted")
 	if err != nil {
 		return err
 	}
-	fmt.Println(sout.String())
+
+	gfIssues, err := issuesByLabel(opts.Repository, "good first issue")
+	if err != nil {
+		return err
+	}
+
+	choices := []issue{}
+
+	seen := map[int]bool{}
+	for _, issueList := range [][]issue{hwIssues, gfIssues} {
+		for _, issue := range issueList {
+			if _, ok := seen[issue.Number]; ok {
+				continue
+			}
+			seen[issue.Number] = true
+
+			blocked := false
+			for _, label := range issue.Labels {
+				blocked = (label.Name == "blocked")
+			}
+			if blocked {
+				continue
+			}
+
+			cutoff, _ := time.ParseDuration("8760h")
+			elapsed := time.Since(issue.CreatedAt)
+			if elapsed > cutoff {
+				continue
+			}
+
+			// TODO filter out ones with pr
+
+			choices = append(choices, issue)
+		}
+	}
+
+	// TODO do the random pick
+	// TODO print nice thing
+	// TODO print url for clicking
+	for _, issue := range choices {
+		fmt.Printf("%d %s\n", issue.Number, issue.Title)
+	}
 
 	return nil
 }
@@ -68,6 +109,32 @@ func main() {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
+}
+
+type label struct {
+	Name string
+}
+
+type issue struct {
+	Number    int
+	Title     string
+	URL       string `json:"url"`
+	CreatedAt time.Time
+	Labels    []label
+}
+
+func issuesByLabel(repository, label string) ([]issue, error) {
+	sout, _, err := gh("issue", "list", "-l", label, "-R", repository, "--json", "number,title,labels,url,createdAt")
+	if err != nil {
+		return nil, err
+	}
+	var result []issue
+	err = json.Unmarshal(sout.Bytes(), &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
 
 // gh shells out to gh, returning STDOUT/STDERR and any error
